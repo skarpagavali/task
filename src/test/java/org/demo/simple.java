94b528ddb1026912af89a30d6b64ca216035cee8
skarpagavali <karpagavalli.s21@gmail.com>
2021-04-14 17:49:14 +0530

    new task

---
+++ src/test/java/org/demo/simple.java
@@ -0,0 +1,15 @@
+package org.demo;
+
+public class simple {
+
+    private void method1(){
+    	System.out.println("method1");
+    }
+    private void method2(){
+    	System.out.println("method2");
+    }
+    private void method3(){
+    	System.out.println("method3");
+    }
+	}
+
